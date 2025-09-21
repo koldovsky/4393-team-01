e14eabc7104f877d4d5adc745961f03e433e50be
ANDRII KHRUSHCH <132401490+Andrew543210@users.noreply.github.com>
2025-09-21 12:51:39 +0000

    solution for js tasks proposal

---
+++ js-tasks/game-move.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move (position, roll) {
+  return position + (roll*2);
+}
---
+++ js-tasks/personalized-message.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet (name, owner) {
+ return (name===owner)?"Hello boss":"Hello guest";
+}
