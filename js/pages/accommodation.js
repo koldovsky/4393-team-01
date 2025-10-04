a191332c91024f0c3cc63bca69cc9a43d54b73b5
Dante <113141702+oleksandrtargaryen@users.noreply.github.com>
2025-10-04 12:31:17 +0000

    added js for button

--- js/pages/accommodation.js
+++ js/pages/accommodation.js
@@ -2,6 +2,15 @@ function init() {
   import("../partials/index.header-menu.js");
   import("../partials/index.footer-social-icons.js");
   import("../partials/index.footer-copyright-year.js");
+  
+  document.querySelectorAll('.accommodation-cabin-rentals__btn').forEach(function(button) {
+    button.addEventListener('click', function() {
+      const target = document.getElementById('choose-the-best');
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
 }
 
 const totalPartials = document.querySelectorAll(
